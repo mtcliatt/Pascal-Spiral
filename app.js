6fec5371c5c86b7768378acb7728a90c6a7cf99a
Matthew Cliatt <mtcliatt@valdosta.edu>
2017-02-23 10:27:49 -0500

    refactoring

--- app.js
+++ app.js
@@ -7,8 +7,7 @@
   - Make input option for resolution (rectsWide & rectsHigh)
   - Make input option for canvas width, height
   - Make input for gap starting and increment
-  - Make util functions
-  - Get rid of grid lines completely, they obsfuscate the code
+  - Add option for drawing 1,2,3,... blocks instead of 2
 
   Notes:
 
@@ -17,93 +16,93 @@
 
 */
 
-let rectangleColor = 'white';
-const gridColor = 'white';
-const gridThickness = 1;
+const colors = [
+  'white',
+  'red',
+]
 
-let resolution = 8000;
-let spiralSize = 4000;
+let colorIndex = 0;
+
+//Good resolutions: 10, 20, 40, 80, 200, 400, 800, 1600, 2000, 4000, 8000
+let resolution = 20;
+let spiralSize = 8000;
 let canvasSize = 800;
+
+let initialGap = 0;
+let gapIncrement = 1;
 
 let utils = {
   clearGrid: null,
   redrawGrid: null,
+  drawRectangle: null,
+  drawRectangleAt: null,
+  rectangleToCoordinate: null,
   getParameters: null,
 };
 
 (function init() {
   const canvas = document.getElementById('canvas');
+  const ctx = canvas.getContext('2d');
 	canvas.width = canvasSize;
 	canvas.height = canvasSize;
 
-  // The canvas's context used for drawing
-  const ctx = canvas.getContext('2d');
-
-  // The room for the rectangles is what is left over after the lines are drawn
   let rectangleSize = (canvasSize - resolution) / resolution;
 
   utils.clearGrid = () => ctx.clearRect(0, 0, canvas.width, canvas.height);
 
-  drawSpiral();
+  utils.redrawGrid = () => {
+    utils.clearGrid();
+    drawSpiral();
+  }
 
-  // Draws a rect from (x, y) to (x + rectangleWidth, y + rectangleHeight)
-  function drawRectangleAt(x, y) {
+  utils.drawRectangleAt = (x, y) => {
     ctx.clearRect(x, y, rectangleSize, rectangleSize);
-    ctx.fillStyle = rectangleColor;
+    ctx.fillStyle = colors[colorIndex % colors.length];
     ctx.fillRect(x, y, rectangleSize, rectangleSize);
   }
 
-  // Wrapper for drawRectangleAt with col,row params instead of x,y
-  function drawRectangle(column, row) {
-    const {x, y} = rectangleToCoordinate(column, row);
-    drawRectangleAt(x, y);
+  utils.drawRectangle = (column, row) => {
+    const {x, y} = utils.rectangleToCoordinate(column, row);
+    if (column % 2 == 1 && row % 2 == 1)
+      colorIndex++;
+    utils.drawRectangleAt(x, y);
   }
 
-  // Just don't even question this.
-  function rectangleToCoordinate(column, row) {
-
-    // The max amount of vertical space rectangles and grid lines could take up
-    const maxRectSpace = (resolution - 1) * rectangleSize;
-    const maxGridSpace = resolution * gridThickness;
-    const maxy = maxRectSpace + maxGridSpace;
-
-    // The actual amount of space, up to the column specified
-    const rectSpaceHorizontal = column * rectangleSize;
-    const gridSpaceHorizontal = (column * gridThickness) + (gridThickness / 2);
-
-    // The actual amount of space, up to the row specified
-    const rectSpaceVertical = row * rectangleSize;
-    const gridSpaceVertical = (row * gridThickness) + (gridThickness / 2);
-    const x = rectSpaceHorizontal + gridSpaceHorizontal;
-    const y = maxy - (rectSpaceVertical + gridSpaceVertical);
+  utils.rectangleToCoordinate = (column, row) => {
+    const x = column * rectangleSize + column;
+    const maxy = (resolution * rectangleSize) + resolution;
+    const y = maxy - (row * rectangleSize + row);
 
     return {x, y};
   }
 
-  function drawSpiral() {
-    let offset = resolution / 2;
-    let gap = 0;
-    let skips = 0;
-    let x = 0;
-    let y = 0;
-    let delta = [0, -1];
-
-    for (let i = Math.pow(spiralSize, 2); i > 0; i--, skips++) {
-
-      // Is it time to draw one of the 1's at the end of the gap?
-      if (skips === gap) {
-        drawRectangle(x + offset, y + offset);
-      } else if (skips === gap + 1) {
-        drawRectangle(x + offset, y + offset);
-        skips = 0;
-        gap += 1;
-      }
-      
-      if (x === y || (x < 0 && x === -y) || (x > 0 && x === 1 - y))
-          delta = [-delta[1], delta[0]];
-
-      x += delta[0];
-      y += delta[1];
-    }
-  }
+  drawSpiral();
 })();
+
+function drawSpiral() {
+  let offset = resolution / 2;
+  let gap = initialGap;
+  let skips = 0;
+  let x = 0;
+  let y = 0;
+  let delta = [0, -1];
+
+  for (let i = Math.pow(spiralSize, 2); i > 0; i--, skips++) {
+
+    // Is it time to draw one of the 1's at the end of the gap?
+    if (skips === gap) {
+      utils.drawRectangle(x + offset, y + offset);
+    } else if (skips === gap + 1) {
+      utils.drawRectangle(x + offset, y + offset);
+      skips = 0;
+      gap += gapIncrement;
+    }
+    
+    if (x === y || (x < 0 && x === -y) || (x > 0 && x === 1 - y))
+      delta = [-delta[1], delta[0]];
+
+    x += delta[0];
+    y += delta[1];
+  }
+}
+
